bb6e97ac231a0c7e588ecd6e0c498f395e42b606
Irina <myr-irina2@yandex.ru>
2022-07-11 09:36:06 +0300

    Delete comments

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,8 +1,4 @@
-// import Favorites from '../../pages/favorites/Favorites';
-// import MainEmpty from '../../pages/main-empty/MainEmpty';
-import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
-// import Login from '../../pages/login/Login';
-// import Main from '../../pages/main/Main';
+import Main from '../../pages/main/Main';
 
 
 type AppScreenProps = {
@@ -11,11 +7,8 @@ type AppScreenProps = {
 
 function App({ placesCount }: AppScreenProps) {
   return (
-    // <Main placesCount={placesCount} />
-    // <Login/>
-    // <Favorites />
-    // <MainEmpty />
-    <NotFoundScreen/>
+    <Main placesCount={placesCount} />
+
   );
 }
 
