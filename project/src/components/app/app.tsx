17d834eb85bad90efe3319f4d2fc6a5e6942310b
Irina <myr-irina2@yandex.ru>
2022-07-12 21:11:19 +0300

    Fix indent in app.tsx

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,15 +1,11 @@
 import Main from '../../pages/main/Main';
 
-
 type AppScreenProps = {
   placesCount: number;
-}
+};
 
 function App({ placesCount }: AppScreenProps) {
-  return (
-    <Main placesCount={placesCount} />
-
-  );
+  return <Main placesCount={placesCount} />;
 }
 
 export default App;
