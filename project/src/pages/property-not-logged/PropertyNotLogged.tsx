4399409ae6c12f127297dcd0b3b5bd2ff252fa95
Irina <myr-irina2@yandex.ru>
2022-08-02 12:38:41 +0300

    Add header component

--- project/src/pages/property-not-logged/PropertyNotLogged.tsx
+++ project/src/pages/property-not-logged/PropertyNotLogged.tsx
@@ -1,31 +1,10 @@
-import Logo from '../../components/logo/logo';
+import Header from '../../components/header/Header';
+
 
 function PropertyNotLogged() {
   return (
     <div className="page">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Logo />
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="/"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__login">Sign in</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--property">
         <section className="property">
           <div className="property__gallery-container container">
