8f245174b07ffbbd23af49bf1c0379529c7f0639
Irina <myr-irina2@yandex.ru>
2022-08-05 07:34:48 +0300

    Add action filteroffers

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -2,3 +2,4 @@ import { createAction } from '@reduxjs/toolkit';
 
 export const setActiveCity = createAction<string>('offers/setActiveCity');
 export const fillOfferList = createAction('offers/fillOfferList');
+export const filterOffers = createAction('offers/filterOffers');
